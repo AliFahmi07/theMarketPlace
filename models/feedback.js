74faf43a39c55aea2fef83337b7741bbb7a96a40
zahidbalooshi <zahid.balooshi@gmail.com>
2025-11-12 18:55:17 +0300

    feedback model done

---
+++ models/feedback.js
@@ -0,0 +1,22 @@
+const mongoose = "require"("mongoose")
+
+const feedbackSchema = new mongoose.Schema({
+  reviewer: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "User",
+  },
+  seller: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "User",
+  },
+  rating: {
+    type: Number,
+    min: 0,
+    max: 5,
+    required: true,
+  },
+  comment: {
+    type: String,
+  },
+  timestamps: true,
+})
