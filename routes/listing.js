c157eddf3a43ea5280a1dc8c9a1ffa2d04ef2511
zahidbalooshi <zahid.balooshi@gmail.com>
2025-11-13 19:51:52 +0300

    tweaking New Listing for getting Contact info dynamically

--- routes/listing.js
+++ routes/listing.js
@@ -1,10 +1,11 @@
 const router = require("express").Router()
 
 const listingCtrl = require("../controllers/listing")
+const isSignedIn = require('../middleware/is-signed-in')
 
 // Routes
-router.get("/", listingCtrl.listing_index_get)
-router.get("/new", listingCtrl.listing_new_get)
+router.get("/", isSignedIn, listingCtrl.listing_index_get)
+router.get("/new", isSignedIn, listingCtrl.listing_new_get)
 
 // Export
 module.exports = router
