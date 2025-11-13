2ac8ecc0d9bdd734febe3cf56ca64154489ad84b
AliFahmi07 <ali.fahmi.a12@gmail.com>
2025-11-13 19:14:14 +0300

    Profile Done-delete

--- routes/profile.js
+++ routes/profile.js
@@ -4,4 +4,11 @@ const isSignedIn = require('../middleware/is-signed-in')
 
 
 router.get('/', isSignedIn, profileCtrl.profile_get)
+router.get('/edit', isSignedIn, profileCtrl.profile_edit_get)
+router.put('/', isSignedIn, profileCtrl.profile_edit_put)
+
+
+
+
+
 module.exports = router
