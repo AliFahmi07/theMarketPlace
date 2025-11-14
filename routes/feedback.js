abaceddae6c44c77f1e0152676a921195fe8a08e
AliFahmi07 <ali.fahmi.a12@gmail.com>
2025-11-14 21:36:43 +0300

    Feedback get and post done

--- routes/feedback.js
+++ routes/feedback.js
@@ -1,6 +1,8 @@
 router = require("express").Router()
 const feedbackCtrl = require("../controllers/feedback")
+const isSignedIn = require('../middleware/is-signed-in')
 
-router.get("/:listingId", feedbackCtrl.profile_Feedback_get)
+router.get("/:ownerId", feedbackCtrl.profile_Feedback_get)
+router.post('/:ownerId', isSignedIn, feedbackCtrl.profile_Feedback_post)
 
 module.exports = router
