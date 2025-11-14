85a0d6f57c47903315c682954376f1c6ea7905aa
Sharmeen <sharmeenmirzaalbaig@gmail.com>
2025-11-14 17:54:06 +0300

    profile updated

--- routes/listing.js
+++ routes/listing.js
@@ -4,10 +4,10 @@ const listingCtrl = require("../controllers/listing")
 const isSignedIn = require('../middleware/is-signed-in')
 
 // Routes
-router.get("/", isSignedIn, listingCtrl.listing_index_get)
+router.get("/", listingCtrl.listing_index_get)
 router.get("/new", isSignedIn, listingCtrl.listing_new_get)
 router.post("/", isSignedIn, listingCtrl.listing_new_post)
-router.get("/:listingId", isSignedIn, listingCtrl.listing_show_get)
+router.get("/:listingId", listingCtrl.listing_show_get)
 
 // Export
 module.exports = router
