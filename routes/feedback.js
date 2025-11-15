5c6ced757f8df1b85e293eca3b48b038dc585686
Sharmeen <sharmeenmirzaalbaig@gmail.com>
2025-11-15 20:04:39 +0300

    view all comments done

--- routes/feedback.js
+++ routes/feedback.js
@@ -2,6 +2,7 @@ router = require("express").Router()
 const feedbackCtrl = require("../controllers/feedback")
 const isSignedIn = require('../middleware/is-signed-in')
 
+router.get("/userFeedback", isSignedIn, feedbackCtrl.profile_showFeedback_get)
 router.get("/:ownerId",feedbackCtrl.profile_Feedback_get)
 router.post('/:ownerId', isSignedIn, feedbackCtrl.profile_Feedback_post)
 
