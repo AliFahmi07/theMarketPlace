e1c75b9e6f3530530de5ace27c299ed7500ef769
AliFahmi07 <ali.fahmi.a12@gmail.com>
2025-11-15 16:58:16 +0300

    access to feedback model on ejs

--- controllers/feedback.js
+++ controllers/feedback.js
@@ -5,14 +5,18 @@ const profile_Feedback_get = async (req,res) => {
   const ownerId = req.params.ownerId
   const listing = await Listing.findById(ownerId).populate("owner")
   const sellerId = listing.owner._id
-    res.render("feedback/profile-feedback.ejs", {listing, sellerId,  owner:listing.owner, Listing})
+  const feedback = await Feedback.find({seller:sellerId}).populate("reviewer")
+    res.render("feedback/profile-feedback.ejs", {listing, sellerId,  owner:listing.owner, Listing, feedback})
 }
 
 const profile_Feedback_post = async (req,res) => {
+  const ownerId = req.params.ownerId
+  const listing = await Listing.findById(ownerId).populate("owner")
+
   req.body.reviewer = req.session.user._id
-  req.body.seller = req.body.owner
+  req.body.seller = listing.owner._id
   await Feedback.create(req.body)
-  res.redirect(`/listings/${req.params.ownerId}`)
+  res.redirect(`/listings/${ownerId}`)
 }
 
 module.exports = {
