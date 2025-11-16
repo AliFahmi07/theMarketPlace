784cb37eb7b2697c78e645dc2713b338079e1b0e
AliFahmi07 <ali.fahmi.a12@gmail.com>
2025-11-16 13:44:12 +0300

    User can now see the seller he commented on

--- controllers/feedback.js
+++ controllers/feedback.js
@@ -38,12 +38,22 @@ const profile_Feedback_post = async (req, res) => {
 
 const profile_showFeedback_get = async (req, res) => {
     const reviewerId = req.session.user._id
-    const reviews = (await Feedback.find({reviewer: reviewerId}).populate("seller"))
+    const reviews = await Feedback.find({reviewer: reviewerId}).populate("reviewer").populate("seller")
     res.render("feedback/my-feedbacks.ejs", {reviews})
+}
+
+const profile_editFeedback_put = async (req,res) => {
+  const reviewerId = await Feedback.findById(req.session.user._id)
+
+  const updatedFeedback = await Feedback.findByIdAndUpdate(
+    reviewerId,
+    req.body,
+  )
+
 }
 
 module.exports = {
   profile_Feedback_get,
   profile_Feedback_post,
-  profile_showFeedback_get
+  profile_showFeedback_get,
 }
