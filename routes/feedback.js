6fb3cbe1701bde69e388c0bf770b5b7c12d2d3b9
AliFahmi07 <ali.fahmi.a12@gmail.com>
2025-11-16 15:34:25 +0300

    user can edit and delete feedbacks

--- routes/feedback.js
+++ routes/feedback.js
@@ -6,4 +6,8 @@ router.get("/userFeedback", isSignedIn, feedbackCtrl.profile_showFeedback_get)
 router.get("/:ownerId",feedbackCtrl.profile_Feedback_get)
 router.post('/:ownerId', isSignedIn, feedbackCtrl.profile_Feedback_post)
 
+router.get("/userFeedback/edit/:id", isSignedIn, feedbackCtrl.profile_editFeedback_get)
+router.put("/userFeedback/edit", isSignedIn, feedbackCtrl.profile_editFeedback_put)
+router.delete("/userFeedback/delete/:id", isSignedIn, feedbackCtrl.profile_deleteFeedback_delete)
+
 module.exports = router
